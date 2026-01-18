9a431b0df1acaadf529944b6714e787ede4e9cf4
kamra-n <ikamihere@gmail.com>
2026-01-18 16:24:24 +0500

    basic server in node js

--- Nodejs_01/index.js
+++ Nodejs_01/index.js
@@ -1,1 +1,25 @@
-console.log('Hello from node js');
+import http, { createServer } from 'http'
+
+
+const server = createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+    switch (req.url) {
+        case '/':
+            res.end('Hello Home Page!\n');
+        case '/about':
+            res.end('Hello From about');
+        default:
+            res.end('Page Not Found')
+
+
+    }
+
+});
+
+
+
+// starts a simple http server locally on port 3000
+server.listen(3000, () => {
+    console.log('Listening on http://localhost:3000');
+});
