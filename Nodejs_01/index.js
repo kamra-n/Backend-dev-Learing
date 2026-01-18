28b442290bcd9d07b70cf4afcfb5a1b17e1758c3
kamra-n <ikamihere@gmail.com>
2026-01-18 16:54:04 +0500

    simple node js server with fs module

--- Nodejs_01/index.js
+++ Nodejs_01/index.js
@@ -1,18 +1,30 @@
-import http, { createServer } from 'http'
+import { createServer } from 'http'
+import fs from 'node:fs';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
+// Get the current file's directory path
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-const server = createServer((req, res) => {
+const server = createServer(async (req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
 
     switch (req.url) {
         case '/':
-            res.end('Hello Home Page!\n');
+            const date = new Date().toISOString();
+
+            await fs.promises.appendFile(__dirname + '/logs.txt', `New Logs from / route at ${date} \n`);
+            res.end('Hello From home');
+            break;
+
         case '/about':
             res.end('Hello From about');
+            break;  // <-- Important!
+
         default:
-            res.end('Page Not Found')
-
-
+            res.end('Page Not Found');
+            break;
     }
 
 });
